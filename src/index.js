ccfaf590db41e8b90d4e81aa7250aa3cf0f1a8e1
imkarimkarim <allalirezamail@gmail.com>
2022-06-03 23:19:26 +0430

    writing the actuall code(persian sort functions)

--- src/index.js
+++ src/index.js
@@ -2,21 +2,145 @@
 
 const { alphabet } = require("./alphabet");
 
-const compare = (a, b) => {
-  if (alphabet[a] > alphabet[b]) {
-    return true;
-  }
-  if (alphabet[a] < alphabet[b]) {
+const isInAlphabet = (letter) => {
+  if (!letter) return false;
+  letter = letter.toString();
+  if (alphabet[letter[0]] === undefined) {
     return false;
   }
-  return false;
+  return true;
 };
+
+// returns(1/-1) are based on official .sort() "compare" option
+const alphabetCompare = (a, b) => {
+  if (!isInAlphabet(a) || !isInAlphabet(b)) {
+    if (isInAlphabet(a)) {
+      return -1;
+    }
+    if (isInAlphabet(b)) {
+      return 1;
+    }
+  }
+  if (alphabet[a] > alphabet[b]) {
+    return 1;
+  }
+  if (alphabet[a] < alphabet[b]) {
+    return -1;
+  }
+  return -1;
+};
+
+// will sorting till 3Letters! (maybe change/opt it later...)
+const wordCompare = (a, b) => {
+  if (a.length === 1 && b.length > 1) {
+    return -1;
+  }
+  if (b.length === 1 && a.length > 1) {
+    return 1;
+  }
+  if (a.length === 1 && b.length === 1) {
+    return alphabetCompare(a[0], b[0]);
+  }
+  // all the first letters by here will be equal(groupByFirstLetter)
+  if (a[1] === b[1] && a.length >= 3 && b.length >= 3) {
+    return alphabetCompare(a[2], b[2]);
+  }
+  if (a.length >= 2 && b.length >= 2) {
+    if (a[1] === b[1]) {
+      if ((a.length === 2 || b.length === 2) && a.length < b.length) {
+        return -1;
+      }
+      return 1;
+    }
+    return alphabetCompare(a[1], b[1]);
+  }
+};
+
+const groupByFirstLetter = (list) => {
+  if (!list) {
+    return false;
+  }
+  let groups = {};
+  for (let i = 0; i < list.length; i++) {
+    let word = list[i];
+    // fL stands for first letter of the word
+    let fL = word[0];
+    if (groups[fL] === undefined) {
+      groups[fL] = [word];
+    } else {
+      groups[fL].push(word);
+    }
+  }
+  return groups;
+};
+
+const sortTheGroups = (groups) => {
+  if (!groups) {
+    return false;
+  }
+  return Object.keys(groups)
+    .sort(alphabetCompare)
+    .reduce((res, key) => {
+      res[key] = groups[key];
+      return res;
+    }, {});
+};
+
+const sortGroupItems = (group) => {
+  if (!group) {
+    return false;
+  }
+  return group.sort(wordCompare);
+};
+
+const pSort = (wordsList) => {
+  if (!wordsList) {
+    return false;
+  }
+  let res = [];
+  wordsList = groupByFirstLetter(wordsList);
+  wordsList = sortTheGroups(wordsList);
+  for (const key in wordsList) {
+    wordsList[key] = sortGroupItems(wordsList[key]);
+    for (let i = 0; i < wordsList[key].length; i++) {
+      res.push(wordsList[key][i]);
+    }
+  }
+  return res;
+};
+
+const test = [
+  "Yazd",
+  "yazd",
+  "یزد",
+  "جادی",
+  "ریکت",
+  "گوگل",
+  "اسلایمم",
+  "ع",
+  "عمه",
+  "ه",
+  "خ",
+  "x",
+  ",",
+  "?",
+  "خاله",
+  "مهشید",
+  "مهشدی",
+  "karim",
+  "w",
+  1,
+  2,
+  3,
+  3,
+  4,
+];
+
+console.log(pSort(test));
 
 // TODO: pSort
 
-// TODO: checking inputs
 // TODO: module...
-// TODO: groupByFirstLetter
-// TODO: sortGroup
 // TODO: share/packaging
 // TODO: readme
+// TODO: seperate persian words from otherwords and others.sort()
